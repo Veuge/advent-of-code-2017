ff66223b7c857dd3655dd91ddcd1ad57292e9391
Veronica Clavijo <veuge.clavijo@gmail.com>
2017-12-20 18:09:18 -0400

    day 1

---
+++ day-1.js
@@ -0,0 +1,15 @@
+function captcha(input, increment, normal){
+    var sum = 0;
+    var i = 0;
+    var multiplier = normal ? 1 : 2;
+
+    while(i < input.length){
+        var current = input[i];
+        var next = (i + increment === input.length && normal) ? input[0] : input[i + increment];
+        if(current === next){
+            sum += (parseInt(current) * multiplier);
+        }
+        i++;
+    }
+    console.log(sum);
+}
